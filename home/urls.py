2bc0dc5d212098b71891b7a37f25c8a3aaf43e60
Shane <shane@shanerice.me>
2019-01-02 23:52:04 -0800

    Updated URLs :link:

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 import home.views as home
 
@@ -7,6 +8,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', home.home_view, name='index'),
+    path('roms/', RedirectView.as_view(url='//roms.smwc.world/', permanent=False), name='roms'),
     path('error/', home.error_view, name='error'),
     path('success/', home.success_view, name='success'),
 ]
