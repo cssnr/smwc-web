a072808537fb07d62fa5a7193a7efdb1fefce2ac
Shane <shane@shanerice.me>
2019-01-13 22:27:11 -0800

    Moved flower url to uri.

--- smwc_web/urls.py
+++ smwc_web/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('oauth/', include('oauth.urls')),
     path('admin/', admin.site.urls),
-    path('flower/', RedirectView.as_view(url='//flower.smwc.world/'), name='flower'),
+    path('flower/', RedirectView.as_view(url='/flower/'), name='flower'),
 ]
 
 if settings.DEBUG:
