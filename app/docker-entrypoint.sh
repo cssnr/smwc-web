db0e1e15a3a882025617103f4dfb9a736caf4700
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-05 12:10:24 -0700

    Update

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if echo "${*}" | grep -q "gun";then
     if [ -n "${DJANGO_SUPERUSER_PASSWORD}" ] &&
     [ -n "${DJANGO_SUPERUSER_USERNAME}" ] &&
     [ -n "${DJANGO_SUPERUSER_EMAIL}" ];then
-        python manage.py createsuperuser --noinput
+        python manage.py createsuperuser --noinput || :
     fi
     python manage.py collectstatic --noinput
 fi
