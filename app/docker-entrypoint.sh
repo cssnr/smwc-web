41b571cfa13ca24f579832d32220249cfd1aa3d5
Shane <6071159+smashedr@users.noreply.github.com>
2025-07-17 22:05:54 -0700

    ADD MIGRATIONS to docker-entrypoint.sh

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-if echo "${*}" | grep -q "gun";then
+if echo "${*}" | grep -q "gun\|runserver";then
+    # TODO: Use flock so this only runs once
+    python manage.py migrate
+    python manage.py collectstatic --noinput -v 0
     if [ -n "${DJANGO_SUPERUSER_PASSWORD}" ] &&
     [ -n "${DJANGO_SUPERUSER_USERNAME}" ] &&
     [ -n "${DJANGO_SUPERUSER_EMAIL}" ];then
         python manage.py createsuperuser --noinput || :
     fi
-    python manage.py collectstatic --noinput
 fi
 
 exec "$@"
